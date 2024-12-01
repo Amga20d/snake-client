5a3672754145f4efa4d38443d25f86b89d1bf48a
Amgad Abdullah <amga200d@gmail.com>
2024-12-01 16:56:27 -0700

    sending the input commands to the connection object to control the snake

--- input.js
+++ input.js
@@ -1,14 +1,27 @@
+// Stores the active TCP connection object.
+let connection;
+
 // handleUserInput function
 
 const handleUserInput = function (key) {
-  if (key === '\u0003') { 
+  if (key === "\u0003") { 
     process.exit(); // terminate the game 
+  } else if (key === "w") { 
+    connection.write("Move: up")
+  } else if (key === 'a') { 
+    connection.write("Move: left")
+  } else if (key === "s") { 
+    connection.write("Move: down")
+  } else if (key === "d") { 
+    connection.write("Move: right")
   }
+  
 };
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
